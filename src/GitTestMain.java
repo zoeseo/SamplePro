a9eac10248031d3128cb95d8e27f32ffd45a91c9
zoeseo <fedoz@naver.com>
2022-03-08 17:15:51 +0900

    Master final 문자열 출력

--- src/GitTestMain.java
+++ src/GitTestMain.java
@@ -4,11 +4,13 @@ public class GitTestMain {
 	public static void main(String[] args) {
 		System.out.println("GitTestMain");
 		System.out.println("Master Branch");
+		
 		System.out.println("hotfix Branch3");
 		System.out.println("hotfix Branch");
 		System.out.println("hotfix Branch2");
 		
 		System.out.println("Master Branch2");
+		System.out.println("Master final");
 	}
 
 }
